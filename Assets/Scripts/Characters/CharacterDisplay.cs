8e258393d042bbd5fa2912154f9cac456223272f
ddmills <mills.dalton@gmail.com>
2020-03-08 18:23:20 -0500

    Sword swoosh effect

--- Assets/Scripts/Characters/CharacterDisplay.cs
+++ Assets/Scripts/Characters/CharacterDisplay.cs
@@ -10,12 +10,15 @@ namespace Sleepy.Characters
         public SpriteRenderer sprite;
         [SerializeField]
         public GameObject bloodEffectPrefab;
+        [SerializeField]
+        public Transform frontEffect;
 
         public void Update()
         {
             Character.Animator.SetFloat("Speed", Character.Body.Speed);
 
             sprite.flipX = Character.Body.MovementDirection.x < 0;
+            frontEffect.localEulerAngles = new Vector3(0, 0, Character.Body.Angle - 90);
         }
 
         public void SpawnBlood()
